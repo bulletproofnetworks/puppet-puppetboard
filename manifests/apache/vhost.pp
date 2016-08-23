fe7a3b4bd8f579dc17d95717edf09d77ea67cdd7
James Mollison <james@bulletproof.net>
2016-08-23 12:38:35 +1000

    undef everything?

--- manifests/apache/vhost.pp
+++ manifests/apache/vhost.pp
@@ -63,11 +63,11 @@ class puppetboard::apache::vhost (
   $group       = $::puppetboard::params::group,
   $basedir     = $::puppetboard::params::basedir,
   $override    = $::puppetboard::params::apache_override,
+  $enable_ldap_auth = $::puppetboard::params::enable_ldap_auth,
   $ldap_bind_dn = $::puppetboard::params::ldap_bind_dn,
   $ldap_bind_password = $::puppetboard::params::ldap_bind_password,
   $ldap_url = $::puppetboard::params::ldap_url,
-  $ldap_bind_authoritative = $::puppetboard::params::ldap_bind_authoritative,
-  $enable_ldap_auth = $::puppetboard::params::enable_ldap_auth
+  $ldap_bind_authoritative = $::puppetboard::params::ldap_bind_authoritative
 
 ) inherits ::puppetboard::params {
 
